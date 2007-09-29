f9b47ee7aec71436da38af65a8caef60de0f27e3
npopuk <npopuk@ee1ccf56-b33a-0410-b18c-fb747e0fe0d7>
2007-09-29 06:19:48 +0000

    Removed file/folder

--- trunk/PocketPC2002_JP/cab.bat
+++
@@ -1,1 +0,0 @@
-"C:\Windows CE Tools\wce300\Pocket PC 2002\support\ActiveSync\windows ce application installation\cabwiz\Cabwiz.exe" nPOP.inf /err err.log /cpu arm
--- trunk/PocketPC_JP/cab.bat
+++
@@ -1,1 +0,0 @@
-"C:\Windows CE Tools\wce300\MS Pocket PC\support\ActiveSync\windows ce application installation\cabwiz\Cabwiz.exe" nPOP.inf /err err.log /cpu mips sh3 arm
