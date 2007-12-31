6599f1ea7899e5a24dd3570f41bae3025248d368
gjcoram@gmail.com <gjcoram@gmail.com@ee1ccf56-b33a-0410-b18c-fb747e0fe0d7>
2007-12-31 09:28:05 +0000

    WCE: option to save all on minimize

--- trunk/src/General.h
+++ trunk/src/General.h
@@ -44,7 +44,7 @@
 #include "font.h"
 
 /* Define */
-#define APP_NAME				TEXT("nPOPuk Ver 2.09b20")
+#define APP_NAME				TEXT("nPOPuk Ver 2.09b21")
 #define APP_VERSION_NUM			2007
 ////////////////////// MRP ////////////////////
 #define HIGH_PRIORITY			TEXT("High")
@@ -328,6 +328,7 @@ typedef struct _OPTION {
 	int ShowTrayIcon;
 	int StartHide;
 	int MinsizeHide;
+	int MinsizeSaveAll;
 	int CloseHide;
 	int TrayIconToggle;
 	int StartInit;
