31dadffe3da73e5de524aeb0cf62336d7ae5c699
npopuk <npopuk@ee1ccf56-b33a-0410-b18c-fb747e0fe0d7>
2008-05-07 22:43:24 +0000

    version 20.10.10.1 - update npop_ver.h to support Release Candidates and patched releases. - add command line to log file - adjust command line parsing to handle WM5/6 options

--- trunk/src/npop_ver.h
+++ trunk/src/npop_ver.h
@@ -2,48 +2,84 @@
 #define _INC_MAIL_NPOP_VER_H
 
 /* First two defines are the major and minor version of nPOPuk.
- * The next two defines should be blank for real releases.
- * Beta and Patch numbers can be placed there when appropriate.
+ *
+ * For a real release, use Beta = 100 and patch = 0 or empty
+ * If there is ever a need to re-release a real-release, use Beta = 100 and patch > 0
+ * For a release candidate, use Beta = 99, Patch = rc#
+ * For a beta, use a Beta between 1 and 98, with Patch = 0 or empty
+ * For a patched beta, use Beta between 1 and 98, with Patch > 0
  */
 #define NPOPMAJOR  2
 #define NPOPMINOR 10
 #define NPOPBETA  10
-#define NPOPPATCH  0
+#define NPOPPATCH  1
 
+// convert text, blanks, or zeroes to zeroes
+#if NPOPMAJOR < 1
+#error NPOPMAJOR must be numeric, 1 or greater
+#endif
+#if NPOPMINOR < 1
+#undef NPOPMINOR
+#define NPOPMINOR 0
+#endif
+#if NPOPBETA < 1
+#undef NPOPBETA
+#define NPOPBETA 0
+#endif
+#if NPOPPATCH < 1
+#undef NPOPPATCH
+#define NPOPPATCH 0
+#endif
 
+#if nPOPBETA == 100
 #if NPOPPATCH
-#define NPOPCALCBETA NPOPBETA
-#define NPOPCALCPATCH NPOPPATCH
-#define MKVERSTRHELP(a,b,c,d) #a "." #b "b" #c "p" #d
-#define MKVERTXTHELP(a,b,c,d) TEXT(#a) TEXT(".") TEXT(#b) TEXT("b") TEXT(#c) TEXT("p") TEXT(#d)
-#define MKVERLNGHELP(a,b,c,d) #a "." #b " Beta " #c " Patch " #d
-#define MKVERSTR(a,b,c,d) MKVERSTRHELP(a,b,c,d)
-#define MKVERTXT(a,b,c,d) MKVERTXTHELP(a,b,c,d)
-#define MKVERLNG(a,b,c,d) MKVERSTRHELP(a,b,c,d)
-#elif NPOPBETA
-#define NPOPCALCBETA NPOPBETA
-#define NPOPCALCPATCH 0
-#define MKVERSTRHELP(a,b,c,d) #a "." #b "b" #c
-#define MKVERTXTHELP(a,b,c,d) TEXT(#a) TEXT(".") TEXT(#b) TEXT("b") TEXT(#c)
-#define MKVERLNGHELP(a,b,c,d) #a "." #b " Beta " #c
-#define MKVERSTR(a,b,c,d) MKVERSTRHELP(a,b,c,d)
-#define MKVERTXT(a,b,c,d) MKVERTXTHELP(a,b,c,d)
-#define MKVERLNG(a,b,c,d) MKVERSTRHELP(a,b,c,d)
-
+// Re-released (patched) version
+#define MKVERSTRHELP(a,b,c,d) #a "." #b "p" #d
+#define MKVERTXTHELP(a,b,c,d) TEXT(#a) TEXT(".") TEXT(#b) TEXT("p") TEXT(#d)
+#define MKVERLNGHELP(a,b,c,d) #a "." #b " patch " #d
 #else
-#define NPOPCALCBETA 0
-#define NPOPCALCPATCH 0
+// Real release
 #define MKVERSTRHELP(a,b,c,d) #a "." #b
 #define MKVERTXTHELP(a,b,c,d) TEXT(#a) TEXT(".") TEXT(#b)
 #define MKVERLNGHELP(a,b,c,d) #a "." #b
+#endif
+
+#elif NPOPBETA == 99
+// Beta releases, with and without patch number
+#if NPOPPATCH
+#define MKVERSTRHELP(a,b,c,d) #a "." #b "rc" #d
+#define MKVERTXTHELP(a,b,c,d) TEXT(#a) TEXT(".") TEXT(#b) TEXT("rc") TEXT(#d)
+#define MKVERLNGHELP(a,b,c,d) #a "." #b " Release Candidate " #d
+#else
+#define MKVERSTRHELP(a,b,c,d) #a "." #b "rc" #d
+#define MKVERTXTHELP(a,b,c,d) TEXT(#a) TEXT(".") TEXT(#b) TEXT("rc") TEXT(#d)
+#define MKVERLNGHELP(a,b,c,d) #a "." #b " Release Candidate " #d
+#endif
+
+#else
+#if NPOPPATCH
+#define MKVERSTRHELP(a,b,c,d) #a "." #b "b" #c "p" #d
+#define MKVERTXTHELP(a,b,c,d) TEXT(#a) TEXT(".") TEXT(#b) TEXT("b") TEXT(#c) TEXT("p") TEXT(#d)
+#define MKVERLNGHELP(a,b,c,d) #a "." #b " Beta " #c " Patch " #d
+#elif NPOPBETA
+#define MKVERSTRHELP(a,b,c,d) #a "." #b "b" #c
+#define MKVERTXTHELP(a,b,c,d) TEXT(#a) TEXT(".") TEXT(#b) TEXT("b") TEXT(#c)
+#define MKVERLNGHELP(a,b,c,d) #a "." #b " Beta " #c
+
+#else
+#define MKVERSTRHELP(a,b,c,d) #a "." #b
+#define MKVERTXTHELP(a,b,c,d) TEXT(#a) TEXT(".") TEXT(#b)
+#define MKVERLNGHELP(a,b,c,d) #a "." #b
+#endif
+#endif
+
 #define MKVERSTR(a,b,c,d) MKVERSTRHELP(a,b,c,d)
 #define MKVERTXT(a,b,c,d) MKVERTXTHELP(a,b,c,d)
 #define MKVERLNG(a,b,c,d) MKVERSTRHELP(a,b,c,d)
-#endif
 
-#define NPOPVER             NPOPMAJOR, NPOPMINOR, NPOPCALCBETA, NPOPCALCPATCH
-#define NPOPVERSTR MKVERSTR(NPOPMAJOR, NPOPMINOR, NPOPCALCBETA, NPOPCALCPATCH)
-#define NPOPVERTXT MKVERTXT(NPOPMAJOR, NPOPMINOR, NPOPCALCBETA, NPOPCALCPATCH)
-#define NPOPVERLNG MKVERLNG(NPOPMAJOR, NPOPMINOR, NPOPCALCBETA, NPOPCALCPATCH)
+#define NPOPVER             NPOPMAJOR, NPOPMINOR, NPOPBETA, NPOPPATCH
+#define NPOPVERSTR MKVERSTR(NPOPMAJOR, NPOPMINOR, NPOPBETA, NPOPPATCH)
+#define NPOPVERTXT MKVERTXT(NPOPMAJOR, NPOPMINOR, NPOPBETA, NPOPPATCH)
+#define NPOPVERLNG MKVERLNG(NPOPMAJOR, NPOPMINOR, NPOPBETA, NPOPPATCH)
 
 #endif
