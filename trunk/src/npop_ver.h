c3de3fcdbe1a32fa4356f11358a3116f4d75632a
npopuk <npopuk@ee1ccf56-b33a-0410-b18c-fb747e0fe0d7>
2008-05-08 18:40:42 +0000

    tweak npop_ver.h to be more flexible, have more checks, and fix the long string version bug that was producing short strings.

--- trunk/src/npop_ver.h
+++ trunk/src/npop_ver.h
@@ -1,18 +1,26 @@
 #ifndef _INC_MAIL_NPOP_VER_H
 #define _INC_MAIL_NPOP_VER_H
 
-/* First two defines are the major and minor version of nPOPuk.
+/* First two defines define what the 3rd part of the version number should
+ * be, for non-beta versions.
  *
- * For a real release, use Beta = 100 and patch = 0 or empty
- * If there is ever a need to re-release a real-release, use Beta = 100 and patch > 0
- * For a release candidate, use Beta = 99, Patch = rc#
+ * The next two defines are the major and minor version of nPOPuk.
+ *
+ * For a real release, use patch = 0 or empty
+ * If there is ever a need to re-release a real-release, use patch > 0
+ * For a release candidate, use Patch = rc# (should be > 0)
  * For a beta, use a Beta between 1 and 98, with Patch = 0 or empty
  * For a patched beta, use Beta between 1 and 98, with Patch > 0
  */
+
+#define NPOPBETA_REAL_RELEASE 100
+#define NPOPBETA_RELEASE_CAND (NPOPBETA_REAL_RELEASE-1)
+
 #define NPOPMAJOR  2
 #define NPOPMINOR 10
 #define NPOPBETA  10
-#define NPOPPATCH  1
+#define NPOPPATCH  3
+
 
 // convert text, blanks, or zeroes to zeroes
 #if NPOPMAJOR < 1
@@ -31,7 +39,7 @@
 #define NPOPPATCH 0
 #endif
 
-#if nPOPBETA == 100
+#if nPOPBETA == NPOPBETA_REAL_RELEASE
 #if NPOPPATCH
 // Re-released (patched) version
 #define MKVERSTRHELP(a,b,c,d) #a "." #b "p" #d
@@ -44,7 +52,7 @@
 #define MKVERLNGHELP(a,b,c,d) #a "." #b
 #endif
 
-#elif NPOPBETA == 99
+#elif NPOPBETA == NPOPBETA_RELEASE_CAND
 // Beta releases, with and without patch number
 #if NPOPPATCH
 #define MKVERSTRHELP(a,b,c,d) #a "." #b "rc" #d
@@ -75,7 +83,7 @@
 
 #define MKVERSTR(a,b,c,d) MKVERSTRHELP(a,b,c,d)
 #define MKVERTXT(a,b,c,d) MKVERTXTHELP(a,b,c,d)
-#define MKVERLNG(a,b,c,d) MKVERSTRHELP(a,b,c,d)
+#define MKVERLNG(a,b,c,d) MKVERLNGHELP(a,b,c,d)
 
 #define NPOPVER             NPOPMAJOR, NPOPMINOR, NPOPBETA, NPOPPATCH
 #define NPOPVERSTR MKVERSTR(NPOPMAJOR, NPOPMINOR, NPOPBETA, NPOPPATCH)
