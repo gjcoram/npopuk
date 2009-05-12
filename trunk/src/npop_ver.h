14c54806f492ccf9844bf28f89345eb346ad859b
gjcoram@gmail.com <gjcoram@gmail.com@ee1ccf56-b33a-0410-b18c-fb747e0fe0d7>
2009-05-12 11:01:49 +0000

    2.12

--- trunk/src/npop_ver.h
+++ trunk/src/npop_ver.h
@@ -18,8 +18,8 @@
 
 #define NPOPMAJOR  2
 #define NPOPMINOR 12
-#define NPOPBETA  12
-#define NPOPPATCH 14
+#define NPOPBETA  100
+#define NPOPPATCH 0
 
 
 // convert text, blanks, or zeroes to zeroes
