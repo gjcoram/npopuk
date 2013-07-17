706f88694ffa42f693dd77fa6d71418337518da4
gjcoram@gmail.com <gjcoram@gmail.com@ee1ccf56-b33a-0410-b18c-fb747e0fe0d7>
2013-07-17 11:11:40 +0000

    3.02p1 - CAB wizard files for WinCE3 (HPC2000)

--- CabWizard/buildcab.bat
+++ CabWizard/buildcab.bat
@@ -1,10 +1,11 @@
 @echo off
 REM
-REM Sample batch file used to compile the CabWiz INF file
+REM Batch file used to compile the CabWiz INF file
 REM
 REM
-REM Replace <CabWiz-Path> with the full path to the CabWiz.exe file
-REM Replace <INF-Path> with the full path to the Sample.INF file
+REM Must use the full path to the CabWiz.exe file
+REM .INF file expected to be in the current directory
+REM List all targets after the /cpu switch
 REM
 
-"C:\Program Files\Windows CE Tools\wce300\hpc2000\support\activesyncsdk\windows ce application installation\cabwiz\CabWiz.exe" nPOPuk.inf /err err.log /cpu HPC_SH3 HPC_MIPS HPCPro_ARM HPCPro_MIPS HPCPro_SH3 HPCPro_SH4
+"C:\Program Files\Windows CE Tools\wce300\hpc2000\support\activesyncsdk\windows ce application installation\cabwiz\CabWiz.exe" nPOPuk.inf /err err.log /cpu HPC_SH3 HPC_MIPS HPCPro_ARM HPCPro_MIPS HPCPro_SH3 HPCPro_SH4 HPC2000_ARM HPC2000_MIPS HPC2000_X86
