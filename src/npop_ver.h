690c2dab25623bf683c9ebe03b2b761cc6ccc6c0
gjcoram@gmail.com <gjcoram@gmail.com@ee1ccf56-b33a-0410-b18c-fb747e0fe0d7>
2013-08-10 11:38:06 +0000

    3.03rc1

--- src/npop_ver.h
+++ src/npop_ver.h
@@ -18,8 +18,8 @@
 
 #define NPOPMAJOR  3
 #define NPOPMINOR  03
-#define NPOPBETA   4
-#define NPOPPATCH  0
+#define NPOPBETA   99
+#define NPOPPATCH  1
 
 
 // convert text, blanks, or zeroes to zeroes
